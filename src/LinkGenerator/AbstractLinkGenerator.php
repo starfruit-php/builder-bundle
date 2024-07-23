544e704432038460ebd3e97658d28eb944f1b295
Nguyen Hoang Anh <hoanghuynh1998@gmail.com>
2024-07-23 17:20:24 +0700

    PREVIEW: check if language document not published

--- src/LinkGenerator/AbstractLinkGenerator.php
+++ src/LinkGenerator/AbstractLinkGenerator.php
@@ -4,11 +4,29 @@ namespace Starfruit\BuilderBundle\LinkGenerator;
 
 use Pimcore\Model\DataObject\ClassDefinition\LinkGeneratorInterface;
 use Starfruit\BuilderBundle\Config\ObjectConfig;
+use Pimcore\Model\Document;
 
 class AbstractLinkGenerator implements LinkGeneratorInterface
 {
     public function generate(object $object, array $params = []): string
     {
+        try {
+            $trace = debug_backtrace();
+            $call = $trace[1];
+            $isPreview = $call['function'] == 'generatePreviewUrl';
+
+            if ($isPreview) {
+                $locale = $params['locale'];
+
+                $existDocument = Document::getByPath("/$locale");
+                if (!($existDocument && $existDocument->getPublished())) {
+                    unset($params['locale']);
+                }
+            }
+        } catch (\Throwable $e) {
+            
+        }
+            
         $objectConfig = new ObjectConfig($object);
         return $objectConfig->getSlug($params);
     }
