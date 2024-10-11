161bff529cdf70ff0878446fefe431ec5c6bf446
Nguyen Hoang Anh <hoanghuynh1998@gmail.com>
2024-10-11 14:51:55 +0700

    EXTENSION: fix pinterest word error

--- src/Extension/ShareSocialExtension.php
+++ src/Extension/ShareSocialExtension.php
@@ -8,7 +8,7 @@ use Starfruit\BuilderBundle\Tool\SystemTool;
 
 class ShareSocialExtension extends AbstractExtension
 {
-    const ALLOW_TYPES = ['facebook', 'twitter', 'linkedin', 'printerest', 'telegram', 'whatsapp'];
+    const ALLOW_TYPES = ['facebook', 'twitter', 'linkedin', 'pinterest', 'telegram', 'whatsapp'];
 
     /**
      * @inheritDoc
