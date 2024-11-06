f1ada30b0ac3f373e27b3d0cf247913a60eaed08
Nguyen Hoang Anh <hoanghuynh1998@gmail.com>
2024-11-06 16:07:19 +0700

    EXTENSION: update listing

--- src/Extension/ListingExtension.php
+++ src/Extension/ListingExtension.php
@@ -4,9 +4,19 @@ namespace Starfruit\BuilderBundle\Extension;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use Knp\Component\Pager\PaginatorInterface;
 
 class ListingExtension extends AbstractExtension
 {
+    protected $paginator;
+
+    public function __construct(
+        PaginatorInterface $paginator
+    )
+    {
+        $this->paginator = $paginator;
+    }
+
     /**
      * @return array|TwigFunction[]
      */
@@ -28,12 +38,29 @@ class ListingExtension extends AbstractExtension
             $list->setCondition($condStr, $condArr);
         }
 
-        $keyParams = ['limit', 'orderKey', 'order', 'unpublished'];
+        $keyParams = ['orderKey', 'order', 'unpublished'];
         foreach ($keyParams as $keyParam) {
             $paramValue = isset($params[$keyParam]) ? $params[$keyParam] : null;
             if ($paramValue) {
                 $list->{'set' . ucfirst($keyParam)}($paramValue);
             }
+        }
+
+
+        $page = isset($params['page']) ? $params['page'] : 0;
+        $limit = isset($params['limit']) ? $params['limit'] : 0;
+        if (!$page && $limit) {
+            $list->setLimit($limit);
+        }
+
+        if ($page && $limit) {
+            $pagination = $this->paginator->paginate(
+                $list,
+                $page,
+                $limit,
+            );
+
+            return compact('list', 'pagination');
         }
 
         return $list;
