e1d58b86487ec86073e1b534839a6ce7398d8536
Nguyen Hoang Anh <hoanghuynh1998@gmail.com>
2024-11-20 15:34:54 +0700

    EXTENSION: add function render editable image for phone+desktop

--- src/Extension/RenderExtension.php
+++ src/Extension/RenderExtension.php
@@ -10,13 +10,12 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Starfruit\BuilderBundle\LinkGenerator\AbstractLinkGenerator;
 use Starfruit\BuilderBundle\Service\EditableService;
 use Starfruit\BuilderBundle\Tool\DocumentTool;
+use Pimcore\Tool\DeviceDetector;
 
 class RenderExtension extends \Twig\Extension\AbstractExtension
 {
-    /**
-     * @var RequestStack $requestStack
-     */
-    private RequestStack $requestStack;
+    private $requestStack;
+    private $deviceDetector;
 
     public function __construct(
         RequestStack $requestStack
@@ -33,6 +32,7 @@ class RenderExtension extends \Twig\Extension\AbstractExtension
             new TwigFunction('builder_render_image', [$this, 'renderImage']),
             new TwigFunction('builder_render_wysiwyg', ['\Starfruit\BuilderBundle\Tool\TextTool', 'formatWysiwyg']),
             new TwigFunction('builder_render_editables', [$this, 'renderEditables']),
+            new TwigFunction('builder_render_editable_image', [$this, 'renderEditableImageByDevice']),
         ];
     }
 
@@ -78,6 +78,78 @@ class RenderExtension extends \Twig\Extension\AbstractExtension
             $infoImage = $image->getMetaData();
             if (!$alt) {
                 $alt = isset($infoImage['alt']) ? $infoImage['alt'] : Config::getWebsiteConfig()['default_image_alt'];
+            }
+            $title = isset($infoImage['title']) ? $infoImage['title'] : '';
+            
+            $link = "<img
+                    src='$url'
+                    class='$class'
+                    alt='$alt'
+                    title='$title'>";
+            return $link;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param array $images - array of image for device types
+     * @param array $params
+     *
+     * keys in $params
+     * @param string `class` - class value of img tag
+     * @param string `alt` - alt value of img tag
+     * @param array `phone` - key of phone image in $images
+     *      keys in `phone`
+     *      @param string `name` - name of field for phone device in $images
+     *      @param string `thumbnail` - name of thumbnail for phone device in $images
+     * @param array `desktop` - key of desktop image in $images
+     *      keys in `desktop`
+     *      @param string `name` - name of field for desktop device in $images
+     *      @param string `thumbnail` - name of thumbnail for desktop device in $images
+     */
+    public function renderEditableImageByDevice(array $images, array $params)
+    {
+        $image = null;
+        $thumbnailName = null;
+
+        $deviceDetector = DeviceDetector::getInstance();
+        $isPhone = DeviceDetector::getInstance()->isPhone();
+        if ($isPhone) {
+            $name = $params['phone']['name'];
+            $thumbnailName = isset($params['phone']['thumbnail']) ? $params['phone']['thumbnail'] : $thumbnailName;
+            $image = $images[$name];
+        } else {
+            $name = $params['desktop']['name'];
+            $thumbnailName = isset($params['desktop']['thumbnail']) ? $params['desktop']['thumbnail'] : $thumbnailName;
+            $image = $images[$name];
+        }
+
+        if ($image) {
+            $class = isset($params['class']) ? $params['class'] : '';
+            $alt = isset($params['alt']) ? $params['alt'] : '';
+
+            if ($image instanceof Thumbnail) {
+                $image = $image->getAsset();
+            }
+            $url = $image->getFullPath();
+
+            if ($thumbnailName) {
+                $thumbnail = Thumbnail\Config::getByName($thumbnailName);
+
+                if ($thumbnail) {
+                    $thumbnailAsset = $image->getThumbnail($thumbnail);
+                    if (!$thumbnailAsset->exists()) {
+                        $deferred = false;
+                        $thumbnailAsset = $image->getThumbnail($thumbnail, $deferred);
+                    }
+                    $url = $thumbnailAsset->getPath();
+                }
+            }
+
+            $infoImage = $image->getMetaData();
+            if (!$alt) {
+                $alt = isset($infoImage['alt']) ? $infoImage['alt'] : (isset(Config::getWebsiteConfig()['default_image_alt']) ? Config::getWebsiteConfig()['default_image_alt'] : '');
             }
             $title = isset($infoImage['title']) ? $infoImage['title'] : '';
             
